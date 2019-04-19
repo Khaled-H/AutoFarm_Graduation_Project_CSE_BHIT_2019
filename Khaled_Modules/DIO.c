0acc0ff6fffdc3191a4b4c40b16f01bef4d5c88a
khaled hussieny <khaled.hussieny@gmail.com>
2019-04-19 17:48:37 +0200

    Update My files

--- Khaled_Modules/DIO.c
+++ Khaled_Modules/DIO.c
@@ -5,6 +5,111 @@
  *      Author: Khaled
  */
 
+#include "DIO.h"
+
+/****************    write a word to a given port    ****************/
+
+extern void MCAL_DIO_WritePort(DIO_Port_Number PortNumber,u32 Value)
+{
+switch (PortNumber)
+{
+	case PORTA :
+	DIO_PORTA.DIO_ODR = Value;
+	break;
+	case PORTB :
+	DIO_PORTB.DIO_ODR = Value;
+	break;
+	case PORTC :
+	DIO_PORTC.DIO_ODR = Value;
+	break;
+}
+}
+
+/****************     set/reset pin of a given  port   *****************/
+
+extern void MCAL_DIO_WritePin(DIO_Port_Number PortNumber,u8 Value , u8 PinNumber)
+{
+switch (PortNumber)
+{
+/******************  set or reset pins of PORTA     ***************/
+	case PORTA :
+	if (Value)
+	{
+		DIO_PORTA.DIO_BSRR |= (1<<PinNumber);
+	}
+	else
+	{
+		DIO_PORTA.DIO_BSRR |= (1<<(PinNumber+16));
+	}
+	break;
+/***************   set or reset pins of PORTB    *********************/
+	case PORTB :
+	if (Value)
+	{
+		DIO_PORTB.DIO_BSRR |= (1<<PinNumber);
+	}
+	else
+	{
+		DIO_PORTB.DIO_BSRR |= (1<<(PinNumber+16));
+	}
+	break;
+/***************** set or reset pins of PORTC    *******************/
+	case PORTC :
+	if (Value)
+	{
+		DIO_PORTC.DIO_BSRR |= (1<<PinNumber);
+	}
+	else
+	{
+		DIO_PORTC.DIO_BSRR |= (1<<(PinNumber+16));
+	}
+	break;
+}
+}
+
+/*********************     read a word from a given port    ********************/
+
+extern MCAL_DIO_tPortWidth MCAL_DIO_ReadPort(DIO_Port_Number PortNumber)
+{
+MCAL_DIO_tPortWidth Input_Value = 0;
+	switch (PortNumber)
+	{
+		case PORTA :
+		Input_Value = DIO_PORTA.DIO_IDR;
+		break;
+		case PORTB :
+		Input_Value = DIO_PORTB.DIO_IDR;
+		break;
+		case PORTC :
+		Input_Value = DIO_PORTC.DIO_IDR;
+		break;
+
+	}
+	return Input_Value;
+}
+
+/*********************     read a pin from a given port      *************************/
+
+extern STD_tLogic MCAL_DIO_ReadPin(DIO_Port_Number PortNumber ,u8 PinNumber)
+{
+STD_tLogic InputPin = 0;
+switch (PortNumber)
+{
+	case PORTA :
+	InputPin = (DIO_PORTA.DIO_IDR & (1 << PinNumber))>> PinNumber;
+	break;
+	case PORTB :
+	InputPin = (DIO_PORTB.DIO_IDR & (1 << PinNumber));
+	InputPin = (InputPin >> PinNumber);
+	break;
+	case PORTC :
+	InputPin = (DIO_PORTC.DIO_IDR & (1<< PinNumber));
+	InputPin = (InputPin >> PinNumber);
+	break;
+}
+return InputPin;
+}
+
 
 
 
--- Khaled_Modules/PORT.h
+++ Khaled_Modules/PORT.h
@@ -8,7 +8,105 @@
 #ifndef PORT_H_
 #define PORT_H_
 
+#include "DIO.h"
+#include "STD_Type.h"
 
+/*************************************************************
+**********************   HAL_SENSORS  ******************
+*************************************************************/
+
+/***   PORT NUMBER  ***/
+#define  HAL_SENSORS_AMONNIA_PORT        (2)
+#define  HAL_SENSORS_TEMPRATURE_PORT     (2)
+#define  HAL_SENSORS_HUMMIDY_PORT        (1)
+#define  HAL_SENSORS_COLEVEL_PORT        (1)
+/***   PIN NUMBER  ***/
+#define  HAL_SENSORS_AMONNIA_PIN         (1)
+#define  HAL_SENSORS_TEMPRATURE_PIN      (0)
+#define  HAL_SENSORS_HUMMIDY_PIN         (7)
+#define  HAL_SENSORS_COLEVEL_PIN         (6)
+
+
+/*************************************************************
+**********************   HAL_LCD  **********************
+*************************************************************/
+
+/***  PORT NUMBER  ***/
+#define  HAL_LCD_RS_PORT        (2)
+#define  HAL_LCD_EN_PORT        (2)
+#define  HAL_LCD_D4_PORT        (2)
+#define  HAL_LCD_D5_PORT        (2)
+#define  HAL_LCD_D6_PORT        (1)
+#define  HAL_LCD_D7_PORT        (1)
+/***   PIN NUMBER  ***/
+#define  HAL_LCD_RS_PIN         (12)
+#define  HAL_LCD_EN_PIN         (13)
+#define  HAL_LCD_D4_PIN         (14)
+#define  HAL_LCD_D5_PIN         (15)
+#define  HAL_LCD_D6_PIN         (08)
+#define  HAL_LCD_D7_PIN         (09)
+
+
+/*************************************************************
+**********************   HAL_SWITCHES  ******************
+*************************************************************/
+
+/***   PORT NUMBER  ***/
+#define  HAL_SWITCHES_MODE_PORT          (1)
+#define  HAL_SWITCHES_SCROLLUP_PORT      (2)
+#define  HAL_SWITCHES_SCROLLDOWN_PORT    (2)
+#define  HAL_SWITCHES_RIGHT_PORT         (2)
+#define  HAL_SWITCHES_LEFT_PORT          (2)
+/***   PIN NUMBER  ***/
+#define  HAL_SWITCHES_MODE_PIN           (1)
+#define  HAL_SWITCHES_SCROLLUP_PIN       (2)
+#define  HAL_SWITCHES_SCROLLDOWN_PIN     (2)
+#define  HAL_SWITCHES_RIGHT_PIN          (2)
+#define  HAL_SWITCHES_LEFT_PIN           (2)
+
+
+/*************************************************************
+**********************   HAL_ACTUATORS  ****************
+*************************************************************/
+
+/***   PORT NUMBER  ***/
+#define  HAL_ACTUATORS_FAN_PORT          (1)
+#define  HAL_ACTUATORS_AIRHOOD_PORT      (1)
+#define  HAL_ACTUATORS_HEATER_PORT       (1)
+/***   PIN NUMBER  ***/
+#define  HAL_ACTUATORS_FAN_PIN           (1)
+#define  HAL_ACTUATORS_AIRHOOD_PIN       (2)
+#define  HAL_ACTUATORS_HEATER_PIN        (3)
+
+
+/*************************************************************
+*******************   HAL_INDICATORS  ******************
+*************************************************************/
+
+/***  SYSTEM  INDICATOR  ***/
+#define  HAL_LED_PORT      (01)
+#define  HAL_BUZZER_PORT   (01)
+#define  HAL_LED_PIN       (10)
+#define  HAL_BUZZER_PIN    (11)
+
+
+/*************************************************************
+**********************   HAL_UART  *********************
+*************************************************************/
+
+/***  UART PINs  ***/
+#define  HAL_UART_TX_PORT (2)
+#define  HAL_UART_RX_PORT (2)
+#define  HAL_UART_TX_PIN  (6)
+#define  HAL_UART_RX_PIN  (7)
+
+
+/********************************************************************
+*****************  HAL_ Functions Prototypes  *****************
+********************************************************************/
+
+/***  PORT/PIN CONFIGURATION   ***/
+extern void MCAL_PORT_Init(void);
 
 
 
