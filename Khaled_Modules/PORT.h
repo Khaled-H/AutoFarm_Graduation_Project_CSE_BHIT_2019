4639ea01e0a3b347edfb50cd7f447e55c45ffcaf
khaled hussieny <khaled.hussieny@gmail.com>
2019-04-20 17:04:45 +0200

    Khaled_Modules --> modified port.h file ,change the hal_modules names according to the system design naming conventions

--- Khaled_Modules/PORT.h
+++ Khaled_Modules/PORT.h
@@ -17,14 +17,14 @@
 *************************************************************/
 
 /***   PORT NUMBER  ***/
-#define  HAL_SENSORS_AMONNIA_PORT        (2)
-#define  HAL_SENSORS_TEMPRATURE_PORT     (2)
+#define  HAL_SENSORS_TEMP_PORT           (2)
 #define  HAL_SENSORS_HUMMIDY_PORT        (1)
+#define  HAL_SENSORS_AMONNIA_PORT        (2)
 #define  HAL_SENSORS_COLEVEL_PORT        (1)
 /***   PIN NUMBER  ***/
-#define  HAL_SENSORS_AMONNIA_PIN         (1)
-#define  HAL_SENSORS_TEMPRATURE_PIN      (0)
+#define  HAL_SENSORS_TEMP_PIN            (0)
 #define  HAL_SENSORS_HUMMIDY_PIN         (7)
+#define  HAL_SENSORS_AMONNIA_PIN         (1)
 #define  HAL_SENSORS_COLEVEL_PIN         (6)
 
 
@@ -33,19 +33,21 @@
 *************************************************************/
 
 /***  PORT NUMBER  ***/
-#define  HAL_LCD_RS_PORT        (2)
-#define  HAL_LCD_EN_PORT        (2)
-#define  HAL_LCD_D4_PORT        (2)
-#define  HAL_LCD_D5_PORT        (2)
-#define  HAL_LCD_D6_PORT        (1)
-#define  HAL_LCD_D7_PORT        (1)
+#define  HAL_LCDRS_PORT                  (2)
+#define  HAL_LCDRW_PORT                  (2)
+#define  HAL_LCDEN_PORT                  (2)
+#define  HAL_LCDD4_PORT                  (2)
+#define  HAL_LCDD5_PORT                  (2)
+#define  HAL_LCDD6_PORT                  (1)
+#define  HAL_LCDD7_PORT                  (1)
 /***   PIN NUMBER  ***/
-#define  HAL_LCD_RS_PIN         (12)
-#define  HAL_LCD_EN_PIN         (13)
-#define  HAL_LCD_D4_PIN         (14)
-#define  HAL_LCD_D5_PIN         (15)
-#define  HAL_LCD_D6_PIN         (08)
-#define  HAL_LCD_D7_PIN         (09)
+#define  HAL_LCDRS_PIN                   (2)
+#define  HAL_LCDRW_PIN                   (2)
+#define  HAL_LCDEN_PIN                   (2)
+#define  HAL_LCDD4_PIN                   (2)
+#define  HAL_LCDD5_PIN                   (2)
+#define  HAL_LCDD6_PIN                   (1)
+#define  HAL_LCDD7_PIN                   (1)
 
 
 /*************************************************************
@@ -53,17 +55,15 @@
 *************************************************************/
 
 /***   PORT NUMBER  ***/
-#define  HAL_SWITCHES_MODE_PORT          (1)
-#define  HAL_SWITCHES_SCROLLUP_PORT      (2)
-#define  HAL_SWITCHES_SCROLLDOWN_PORT    (2)
-#define  HAL_SWITCHES_RIGHT_PORT         (2)
-#define  HAL_SWITCHES_LEFT_PORT          (2)
+#define  HAL_SWITCHES_MODESW_PORT          (1)
+#define  HAL_SWITCHES_OKSW_PORT            (2)
+#define  HAL_SWITCHES_PLUS_PORT            (2)
+#define  HAL_SWITCHES_MINUSSW_PORT         (2)
 /***   PIN NUMBER  ***/
-#define  HAL_SWITCHES_MODE_PIN           (1)
-#define  HAL_SWITCHES_SCROLLUP_PIN       (2)
-#define  HAL_SWITCHES_SCROLLDOWN_PIN     (2)
-#define  HAL_SWITCHES_RIGHT_PIN          (2)
-#define  HAL_SWITCHES_LEFT_PIN           (2)
+#define  HAL_SWITCHES_MODESW_PIN           (1)
+#define  HAL_SWITCHES_OKSW_PIN             (2)
+#define  HAL_SWITCHES_PLUS_PIN             (2)
+#define  HAL_SWITCHES_MINUSSW_PIN          (2)
 
 
 /*************************************************************
@@ -71,13 +71,13 @@
 *************************************************************/
 
 /***   PORT NUMBER  ***/
-#define  HAL_ACTUATORS_FAN_PORT          (1)
-#define  HAL_ACTUATORS_AIRHOOD_PORT      (1)
-#define  HAL_ACTUATORS_HEATER_PORT       (1)
+#define  HAL_ACTUATORS_UPPERFAN_PORT           (1)
+#define  HAL_ACTUATORS_LOWERFAN_PORT           (1)
+#define  HAL_ACTUATORS_HEATER_PORT             (1)
 /***   PIN NUMBER  ***/
-#define  HAL_ACTUATORS_FAN_PIN           (1)
-#define  HAL_ACTUATORS_AIRHOOD_PIN       (2)
-#define  HAL_ACTUATORS_HEATER_PIN        (3)
+#define  HAL_ACTUATORS_UPPERFAN_PIN            (2)
+#define  HAL_ACTUATORS_LOWERFAN_PIN            (1)
+#define  HAL_ACTUATORS_HEATER_PIN              (3)
 
 
 /*************************************************************
@@ -85,21 +85,25 @@
 *************************************************************/
 
 /***  SYSTEM  INDICATOR  ***/
-#define  HAL_LED_PORT      (01)
-#define  HAL_BUZZER_PORT   (01)
-#define  HAL_LED_PIN       (10)
-#define  HAL_BUZZER_PIN    (11)
+#define  HAL_NORMALMODE_LED_PORT       (01)
+#define  HAL_OTA_LED_PORT              (01)
+#define  HAL_BUZZER_PORT               (01)
+/***   PIN NUMBER  ***/
+#define  HAL_NORMALMODE_LED_PIN        (01)
+#define  HAL_OTA_LED_PIN               (01)
+#define  HAL_BUZZER_PIN                (01)
 
 
 /*************************************************************
 **********************   HAL_UART  *********************
 *************************************************************/
 
-/***  UART PINs  ***/
-#define  HAL_UART_TX_PORT (2)
-#define  HAL_UART_RX_PORT (2)
-#define  HAL_UART_TX_PIN  (6)
-#define  HAL_UART_RX_PIN  (7)
+/***   PORT NUMBER  ***/
+#define  HAL_WIFI_TX_PORT              (2)
+#define  HAL_WIFI_RX_PORT              (2)
+/***   PIN NUMBER   ***/
+#define  HAL_WIFI_TX_PIN               (6)
+#define  HAL_WIFI_RX_PIN               (7)
 
 
 /********************************************************************
