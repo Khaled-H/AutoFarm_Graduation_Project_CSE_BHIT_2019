98eba794defa464da919550308eb81177d6d6b3a
manardyab <manardyab178@gmail.com>
2019-04-23 20:30:13 +0200

    Manar_Modules>>update LCD modules

--- Manar_Modules/LCD.c
+++ Manar_Modules/LCD.c
@@ -14,7 +14,6 @@
  */
 #include "DIO.h"
 #include "LCD.h"
-#include <util/delay.h>
 
 //**** Register Select (RS) Pin Conditions *****
 #define DATA 1
@@ -24,53 +23,51 @@
 #define WRITE 0
 #define READ 1
 
+void _delay_ms(u8 DelayTime){
+	for (int i=0;i<=DelayTime;i++);
+}
 
-
-extern void LCD_voidInit(void)
+extern void HAL_LCD_Init(void)
 {
-DIO_voidSetPortDirection(LCD_DataPort,PORTOUT);
-DIO_voidSetPinDirection(LCD_ControlPort,LCD_EnablePin,PINOUT);
-DIO_voidSetPinDirection(LCD_ControlPort,LCD_ReadWritePin,PINOUT);
-DIO_voidSetPinDirection(LCD_ControlPort,LCD_RegSelectPin,PINOUT);
 
 _delay_ms(40);
-LCD_voidWriteCommand(0b00111000);
+HAL_LCD_WriteCommand(0b00111000);
 _delay_ms(1);
-LCD_voidWriteCommand(0b00001100);
+HAL_LCD_WriteCommand(0b00001100);
 _delay_ms(1);
-LCD_voidWriteCommand(0x01);
+HAL_LCD_WriteCommand(0x01);
 _delay_ms(2);
 
 }
 //************************************************
-extern void LCD_voidWriteChar(u8 Value)
+extern void HAL_LCD_WriteChar(u8 Value)
 {
-DIO_voidWritePort(LCD_DataPort,Value);
-DIO_voidWritePin(LCD_ControlPort,LCD_RegSelectPin,DATA);
-DIO_voidWritePin(LCD_ControlPort,LCD_ReadWritePin,WRITE);
-DIO_voidWritePin(LCD_ControlPort,LCD_EnablePin,BITHIGH);
+MCAL_DIO_WritePort(HAL_LCD_DATA_PORT,Value);
+MCAL_DIO_WritePin(HAL_LCD_CONTROL_PORT,HAL_LCD_RS_PIN,DATA);
+MCAL_DIO_WritePin(HAL_LCD_CONTROL_PORT,HAL_LCD_RW_PIN,WRITE);
+MCAL_DIO_WritePin(HAL_LCD_CONTROL_PORT,HAL_LCD_EN_PIN,BITHIGH);
 _delay_ms(1);
-DIO_voidWritePin(LCD_ControlPort,LCD_EnablePin,BITLOW);
+MCAL_DIO_WritePin(HAL_LCD_CONTROL_PORT,HAL_LCD_EN_PIN,BITLOW);
 }
 //************************************************
 extern void LCD_voidWriteCommand(u8 Value)
 {
-    DIO_voidWritePort(LCD_DataPort,Value);
-    DIO_voidWritePin(LCD_ControlPort,LCD_RegSelectPin,COMMAND);
-    DIO_voidWritePin(LCD_ControlPort,LCD_ReadWritePin,WRITE);
-    DIO_voidWritePin(LCD_ControlPort,LCD_EnablePin,BITHIGH);
+	MCAL_DIO_WritePort(HAL_LCD_DATA_PORT,Value);
+	MCAL_DIO_WritePin(HAL_LCD_CONTROL_PORT,HAL_LCD_RS_PIN,COMMAND);
+	MCAL_DIO_WritePin(HAL_LCD_CONTROL_PORT,HAL_LCD_RW_PIN,WRITE);
+	MCAL_DIO_WritePin(HAL_LCD_CONTROL_PORT,HAL_LCD_EN_PIN,BITHIGH);
     _delay_ms(1);
-    DIO_voidWritePin(LCD_ControlPort,LCD_EnablePin,BITLOW);
+    MCAL_DIO_WritePin(HAL_LCD_CONTROL_PORT,HAL_LCD_EN_PIN,BITLOW);
 
 }
 //**************************************************
 /* Function for Writing A String of ASCIIs Chars on the LCD */
-extern void LCD_voidWriteString(u8 *arry)
+extern void HAL_LCD_WriteString(u8 *arry)
 {
     u8 i =0 ;
     while((arry[i])!= '\0') // loop the String until find the Null Value
     {
-        LCD_voidWriteChar(arry[i]);
+    	HAL_LCD_WriteChar(arry[i]);
         i++;
     }
 }
@@ -80,15 +77,15 @@ extern void LCD_voidWriteString(u8 *arry)
 
 }*/
 //*****************************************************
-extern void LCD_voidWriteNumber(u8 Value)
+extern void HAL_LCD_WriteNumber(u8 Value)
 {
     //      Function for Writing an Integer on the LCD, Converting from Integer
     //      Value to it's ASCII Value
 
     u8 temp = Value%100 ;
     Value = Value /100;
-    LCD_voidWriteChar(Value+48);
+    HAL_LCD_WriteChar(Value+48);
     //Value = Value*10;
-    LCD_voidWriteChar((temp/10)+48);
-    LCD_voidWriteChar((temp%10)+48);
+    HAL_LCD_WriteChar((temp/10)+48);
+    HAL_LCD_WriteChar((temp%10)+48);
 }
--- Manar_Modules/LCD.h
+++ Manar_Modules/LCD.h
@@ -17,17 +17,17 @@
 
 #ifndef _LCD_H__
 #define _LCD_H__
-#include "Types_Utilities.h"
-#include "LCD_Config.h"
+#include "STD_TYPES.h"
+#include "DIO.h"
+#include "PORT.h"
+#define HAL_LCD_CLEAR HAL_LCD_WriteCommand(0x01) // Macro for Clearin the LCD
 
-#define LCD_CLEAR LCD_voidWriteCommand(0x01) // Macro for Clearin the LCD
-
-extern void LCD_voidInit();
-extern void LCD_voidWriteChar(u8 Value);
-extern void LCD_voidWriteCommand(u8 Value);
-extern void LCD_voidWriteString(u8 *arry);
-extern void LCD_voidWriteNumber(u8 Value);
-
+extern void HAL_LCD_Init();
+extern void HAL_LCD_WriteChar(u8 Value);
+extern void HAL_LCD_WriteCommand(u8 Value);
+extern void HAL_LCD_WriteString(u8 *arry);
+extern void HAL_LCD_WriteNumber(u8 Value);
+void _delay_ms(u8 DelayTime);
 
 
 #endif /* LCD_H_ */
