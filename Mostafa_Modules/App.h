14d9c2f2a1926f51bce7f89dc1fe30b6ce123894
Mostafa Samir Nawar <Mostafa.nwar@yahoo.com>
2019-04-24 14:09:08 +0200

    Mostafa_Modules --> Added Application Algorithm Files

---
+++ Mostafa_Modules/App.h
@@ -0,0 +1,18 @@
+/*
+ * App.h
+ *
+ *  Created on: Apr 24, 2019
+ *      Author: Mostafa Samir Nawar		Mostafa.nwar@yahoo.com
+ */
+
+#ifndef APP_H_
+#define APP_H_
+
+#include "STD_TYPES.h"
+#include "SENSORSREADER.h"
+#include "DISPALY.h"
+#include "WIFI.h"
+#include "PROCESS.h"
+
+
+#endif /* APP_H_ */
