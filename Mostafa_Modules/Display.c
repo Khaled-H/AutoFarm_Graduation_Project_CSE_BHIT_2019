9e7cc7394679a55e7eb9f031883fe0859ba25940
Mostafa Samir Nawar <Mostafa.nwar@yahoo.com>
2019-04-24 13:46:31 +0200

    Mostafa_Modules-->> Added Dispaly Module Files

---
+++ Mostafa_Modules/Display.c
@@ -0,0 +1,29 @@
+/*
+ * Display.c
+ *
+ *  Created on: Apr 24, 2019
+ *      Author: Mostafa Samir Nawar		Mostafa.nwar@yahoo.com
+ */
+
+#include "DISPALY.h"
+
+extern void APP_DISPLAY_INIT(void){
+  HAL_LCD_Init();
+}
+
+/*Task to Come every 15 ms to refresh the LCD Display*/
+extern void APP_DISPLAY_Task(void){
+  HAL_LCD_SetCursor(HAL_LCD_FIRSTROW, HAL_LCD_FIRSTCOLUMN);
+  HAL_LCD_WriteString("Temp:");
+  HAL_LCD_WriteChar(APP_SENSORREADER_GetTempLvl());
+  HAL_LCD_WriteString("   ");
+  HAL_LCD_WriteString("Hum:");
+  HAL_LCD_WriteChar(APP_SENSORREADER_GetHumLvl());
+  HAL_LCD_SetCursor(HAL_LCD_SECONDROW, HAL_LCD_FIRSTCOLUMN);
+  HAL_LCD_WriteString("Amonia:");
+  HAL_LCD_WriteChar(APP_SENSORREADER_GetAmoniaLvl());
+  HAL_LCD_WriteString("  ");
+  HAL_LCD_WriteString("CO:");
+  HAL_LCD_WriteChar(APP_SENSORREADER_GetCo2Lvl());
+}
+
