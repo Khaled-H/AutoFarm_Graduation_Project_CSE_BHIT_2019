c0b6533ed1b7f366b69b6ac854c73fe04c3a7be6
manardyab <manardyab178@gmail.com>
2019-04-27 00:31:37 +0200

    Mostafa_Modules-->>modify sensor file .c -->> function HAL_SENSOR_GetTempLV &HAL_SENSOR_GetHumLV

--- Mostafa_Modules/Sensors.c
+++ Mostafa_Modules/Sensors.c
@@ -18,14 +18,31 @@ HAL_SENSORS_tAmoniaLvl HAL_SENSORS_GetAmoniaLvl(void){
 	return temp ;
 }
 HAL_SENSORS_tTempLvl HAL_SENSORS_GetTempLvl(void){
-	HAL_SENSORS_tTempLvl temp ;
-	temp = MCAL_ADC_ReadChannel(HAL_SENSORS_TEMPCHANNEL);
-	return temp ;
+	HAL_SENSORS_tTempLvl  tempDC=0 , tempBI;
+	 u8 rem ,base=1;
+	tempBI = (MCAL_ADC_ReadChannel(HAL_SENSORS_TEMPCHANNEL)&&((u32)0b00000000000000001111111111111111));
+	 while (tempBI> 0)
+	    {
+	        rem = tempBI % 10;
+	        tempDC = tempDC + rem * base;
+	        tempBI = tempBI / 10 ;
+	        base = base * 2;
+	    }
+	return (tempDC / 10);
 }
 HAL_SENSORS_tHumLvl HAL_SENSORS_GetHumLvl(void){
-	HAL_SENSORS_tHumLvl temp ;
-	temp = MCAL_ADC_ReadChannel(HAL_SENSORS_HUMCHANNEL);
-	return temp ;
+	HAL_SENSORS_tTempLvl  humDC=0 , humBI;
+		 u8 rem ,base=1;
+		humBI = (MCAL_ADC_ReadChannel(HAL_SENSORS_HUMCHANNEL)&&((u32)0b11111111111111110000000000000000));
+		humBI = (humBI>>16);
+		 while (humBI> 0)
+		    {
+		        rem = humBI % 10;
+		        humDC = humDC + rem * base;
+		        humBI = humBI / 10 ;
+		        base = base * 2;
+		    }
+		return (humDC / 10);
 }
 HAL_SENSORS_tCo2Lvl HAL_SENSORS_GetCo2Lvl(void){
 	HAL_SENSORS_tCo2Lvl temp ;
