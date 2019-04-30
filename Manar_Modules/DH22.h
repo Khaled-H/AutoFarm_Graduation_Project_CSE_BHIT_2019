3f9b76c32bfcd9baaaaca08a8440f3edd393ea50
Mostafa Samir Nawar <Mostafa.nwar@yahoo.com>
2019-04-30 20:00:23 +0200

    Manar_Modules --> Modified DH22 Files to Work properly

--- Manar_Modules/DH22.h
+++ Manar_Modules/DH22.h
@@ -1,7 +1,7 @@
 /*
  * DH22.h
  *
- *  Created on: ??þ/??þ/????
+ *  Created on: ??ï¿½/??ï¿½/????
  *      Author: manar
  */
 
@@ -14,15 +14,11 @@
 
 // DHT Pin Configuration
 
-#define HAL_DHT_PINOUT  DIO_PORTA.DIO_CRL |= 1 << 17;
-#define HAL_DHT_PININ   DIO_PORTA.DIO_CRL |= 8 << 16;
 
 typedef struct {
 	u16 Temp ;
 	u16 Hum ;
 }HAL_SENSORS_tDH22ReturnType;
 
-
 extern HAL_SENSORS_tDH22ReturnType HAL_DHT22_Read(void);
-static u8 ReadByte(void);
 #endif /* DH22_H_ */
--- Manar_Modules/dh22.c
+++ Manar_Modules/dh22.c
@@ -1,34 +1,33 @@
 /*
  * dh22.c
  *
- *  Created on: ??þ/??þ/????
+ *  Created on: ??ï¿½/??ï¿½/????
  *      Author: manar
  */
 
 
 #include "DH22.h"
+static u8 ReadByte(void);
+static void Delay_us(u8 DelayPeriod);
 
-#define HAL_DH22_DELAY_500uS (500)
-#define HAL_DH22_DELAY_40uS (40)
+#define HAL_DHT_PINOUT  DIO_PORTA.DIO_CRL|=1<<17
+#define HAL_DHT_PININ   DIO_PORTA.DIO_CRL|=8<<16
 
-typedef struct {
-	u16 Temp ;
-	u16 Hum ;
-}HAL_SENSORS_tDH22ReturnType;
+
 
 extern HAL_SENSORS_tDH22ReturnType HAL_DHT22_Read(void){
 	HAL_SENSORS_tDH22ReturnType Ret;
 	u8 Checksum;
 	HAL_DHT_PINOUT;
 	 MCAL_DIO_WritePin(HAL_SENSORS_DHT_PORT,HAL_SENSORS_DHT_CHANNEL,STD_LOW);
-	 Delay_us(HAL_DH22_DELAY_500uS);
+	 Delay_us(500);
 	 MCAL_DIO_WritePin  (HAL_SENSORS_DHT_PORT,HAL_SENSORS_DHT_CHANNEL,STD_HIGH);
-	 Delay_us(HAL_DH22_DELAY_40uS);
+	 Delay_us(40);
 	 HAL_DHT_PININ;
-	 	while(MCAL_DIO_ReadPin(HAL_SENSORS_DHT_PORT,HAL_SENSORS_DHT_CHANNEL) == 1 );
-	 	while(MCAL_DIO_ReadPin(HAL_SENSORS_DHT_PORT,HAL_SENSORS_DHT_CHANNEL) == 0 );
+	 	while(MCAL_DIO_ReadPin(HAL_SENSORS_DHT_PORT,HAL_SENSORS_DHT_CHANNEL) == STD_HIGH );
+	 	while(MCAL_DIO_ReadPin(HAL_SENSORS_DHT_PORT,HAL_SENSORS_DHT_CHANNEL) == STD_LOW );
 	 			/*Response is Correct*/
-		while(MCAL_DIO_ReadPin(HAL_SENSORS_DHT_PORT,HAL_SENSORS_DHT_CHANNEL) == 1 );
+		while(MCAL_DIO_ReadPin(HAL_SENSORS_DHT_PORT,HAL_SENSORS_DHT_CHANNEL) == STD_HIGH );
 		 Ret.Hum = (u16)ReadByte();
 		 Ret.Hum=(Ret.Hum<<8) | ((u16)ReadByte());
 		 Ret.Temp = (u16)ReadByte();
@@ -44,14 +43,14 @@ static u8 ReadByte(void){
 	for (i=0;i<8;i++){
 		while(!(MCAL_DIO_ReadPin(HAL_SENSORS_DHT_PORT,HAL_SENSORS_DHT_CHANNEL)));
 		Delay_us(50);
-		if ((MCAL_DIO_ReadPin(HAL_SENSORS_DHT_PORT,HAL_SENSORS_DHT_CHANNEL)) == 0)   // if the pin is low
+		if ((MCAL_DIO_ReadPin(HAL_SENSORS_DHT_PORT,HAL_SENSORS_DHT_CHANNEL)) == STD_LOW)   // if the pin is low
 			{
 				temp= temp & ~(1<<(8-i));   // write 0
 			}
 			else{
 				temp = temp | (1<<(8-i));  // if the pin is high, write 1
 				}
-			while ((MCAL_DIO_ReadPin(HAL_SENSORS_DHT_PORT,HAL_SENSORS_DHT_CHANNEL)==1));  // wait for the pin to go low
+			while ((MCAL_DIO_ReadPin(HAL_SENSORS_DHT_PORT,HAL_SENSORS_DHT_CHANNEL)==STD_HIGH));  // wait for the pin to go low
 		}
 		return temp;
 	}
@@ -62,10 +61,3 @@ static void Delay_us(u8 DelayPeriod){
 		for(j=0;j<=4;j++);
 	}
 }
-
-
-
-
-
-
-
