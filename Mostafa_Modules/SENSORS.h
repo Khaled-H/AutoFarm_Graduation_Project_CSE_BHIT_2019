b0623661880a22985677489a9dec7c8835bfa7a8
khaled hussieny <khaled.hussieny@gmail.com>
2019-05-02 14:26:11 +0200

    Modified sensor module

--- Mostafa_Modules/SENSORS.h
+++ Mostafa_Modules/SENSORS.h
@@ -12,15 +12,15 @@
 #include "DH22.h"
 /************ TYPES SIZES NEED TO BE REVIEWED BY HARDWARE MAN >> Khaled      ***************/
 #define MINMUM_CO_LVL             (200)
-#define MINMUM_AMMONIIA_LVL       (10)
-#define MQ135_MAX_RES_RATIO       (3.6)
-#define MQ9_MAX_RES_RATIO         (9.9)
+#define MINMUM_AMMONIIA_LVL       (100)
+#define MQ135_MAX_RES_RATIO         (4)
+#define MQ9_MAX_RES_RATIO          (10)
 
-typedef u8 HAL_SENSORS_tAmoniaLvl;
-typedef u8 HAL_SENSORS_tCo2Lvl;
+typedef u16 HAL_SENSORS_tAmoniaLvl;
+typedef u16 HAL_SENSORS_tCo2Lvl;
 
-typedef u8 HAL_SENSORS_tTempLvl;
-typedef u8 HAL_SENSORS_tHumLvl;
+typedef u16 HAL_SENSORS_tTempLvl;
+typedef u16 HAL_SENSORS_tHumLvl;
 
 extern void HAL_SENSORS_Init(void);
 extern HAL_SENSORS_tAmoniaLvl HAL_SENSORS_GetAmoniaLvl(void);
